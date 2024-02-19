085c7661ca1d69e26cf8dc15f33a50d5b72b244c
zaidf <zaidf@DESKTOP-5AO8F19>
2024-02-19 12:49:50 -0600

    Agregar archivos de proyecto.

---
+++ Models/empleado.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace zaidproyecto1.Models
+{
+    [Table("Empleado")]
+    public class empleado
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string nombreE { get; set; }
+
+        public string apellidoE { get; set; }
+
+        public int NumeroE { get; set; }
+
+        public string TrabajoE { get; set; }
+
+        public virtual jefe Jefe { get; set; }
+
+    }
+}
---
+++ Models/jefe.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace zaidproyecto1.Models
+{
+    [Table("JefeEmpresa")]
+    public class jefe
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int Id { get; set; }
+
+        public string TrabajoR { get; set; }
+    }
+}
